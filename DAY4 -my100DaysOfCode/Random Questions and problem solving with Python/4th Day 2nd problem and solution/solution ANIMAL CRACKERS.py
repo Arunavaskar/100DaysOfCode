ae1f9d662bc50aabff5a5e4f4ba8fcf85c87157c
Arunava Chakraborty <36768175+Arunavaskar@users.noreply.github.com>
2018-08-29 23:05:10 +0530

    Add files via upload

---
+++ DAY4 -my100DaysOfCode/Random Questions and problem solving with Python/4th Day 2nd problem and solution/solution ANIMAL CRACKERS.py
@@ -0,0 +1,9 @@
+#ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with same letter
+#animal_crackers('Levelheaded Llama') --> True
+#animal_crackers('Crazy Kangaroo') --> False
+
+#||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+def animal_crackers(text):
+    new=text.split()
+    return new[0:0]==new[0:1]
